7d2708286285e0c1dd6e36e3c4680a9b9fe2501f
lihaiming <lihaiming@bytedance.com>
2023-04-06 17:49:53 +0800

    增加 which-key 插件，方便查看 快捷键，并且附带四个有用的插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -65,6 +65,20 @@ return require('packer').startup(function(use)
     --     end,
     -- })
 
+    -- which-key
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
     use {
         'romgrk/barbar.nvim',
         requires = 'kyazdani42/nvim-web-devicons',
