53ffb02167de4204da26c0038a377265264ca3ae
lihaiming <lihaiming@bytedance.com>
2023-04-10 20:09:07 +0800

    修复 ranger 无法打开 html 文件的问题

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ cp -v ./cpp_farignore ${INSALL_DIR}
 mkdir -pv ${INSALL_DIR}/lua
 cp -v ./lua/*.lua ${INSALL_DIR}/lua/
 
+mkdir -pv ~/.config/ranger/
+cp -v rifle.conf ~/.config/ranger
+
 # 放到 ~/.zshrc 中
 #export OPENAI_API_KEY=xxxxx
 echo "确认 ChatGPT 的 api key 设置正确，到笔记里查找"
